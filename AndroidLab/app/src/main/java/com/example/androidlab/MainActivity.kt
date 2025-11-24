325760edb2883a1f796a152bee2e7b334c193ac9
JYP <john9803@likelion.org>
2025-11-24 21:36:04 +0900

    feat: 메인화면 레이아웃 구성, 버튼생성, 이미지삽입

--- AndroidLab/app/src/main/java/com/example/androidlab/MainActivity.kt
+++ AndroidLab/app/src/main/java/com/example/androidlab/MainActivity.kt
@@ -3,15 +3,34 @@ package com.example.androidlab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -57,7 +76,40 @@ fun LottoNavHost(navController: NavHostController) {
 // ----------- 메인 화면 -----------
 @Composable
 fun MainScreen(onStartClick: () -> Unit) {
+    Column(
+        modifier = Modifier.run {
+            fillMaxSize()
+                .padding(24.dp)
+        },
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Lotto 번호 추출기",
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
 
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.lotto_icon ),
+            contentDescription = "로또 아이콘",
+            modifier = Modifier
+                .size(160.dp)
+                .clip(CircleShape)
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Button(
+            onClick = onStartClick,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("번호 추출", fontSize = 18.sp)
+        }
+    }
 }
 
 // ----------- 결과 화면 -----------
